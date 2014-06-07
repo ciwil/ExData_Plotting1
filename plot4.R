081de97f49ab643e38e507f62f0a2631aa2598ec
czapskiprzemyslaw <ciwil@tlen.pl>
2014-06-07 18:44:32 +0200

    last one

---
+++ plot4.R
@@ -0,0 +1,38 @@
+household_power_consumption <- read.csv("~/Coursera/Exploratory Data Analysis/household_power_consumption.txt", sep=";")
+
+str(household_power_consumption)
+
+DF <- subset(household_power_consumption,Date == "1/2/2007" | Date =="2/2/2007")
+str (DF)
+
+DF$datetime <- strptime(paste0 (DF$Date," ", DF$Time) ,"%d/%m/%Y %H:%M:%S")
+
+
+DF$Global_active_power <- as.numeric(levels(DF$Global_active_power))[DF$Global_active_power]
+DF$Global_reactive_power <- as.numeric(levels(DF$Global_reactive_power))[DF$Global_reactive_power]
+DF$Voltage <- as.numeric(levels(DF$Voltage))[DF$Voltage]
+DF$Global_intensity <- as.numeric(levels(DF$Global_intensity))[DF$Global_intensity]
+DF$Sub_metering_1 <- as.numeric(levels(DF$Sub_metering_1))[DF$Sub_metering_1]
+DF$Sub_metering_2 <- as.numeric(levels(DF$Sub_metering_2))[DF$Sub_metering_2]
+
+
+png("plot4.png", width = 480, height = 480)
+
+par(mfrow=c(2,2))
+# plot TL
+plot(DF$datetime,DF$Global_active_power,type = "l",ylab ="Global Active Power (kilowatts)", xlab = NA)
+
+#plot TR
+plot(DF$datetime,DF$Voltage,type = "l",ylab ="Voltage", xlab = "datetime")
+# plot LL
+with(DF, plot(datetime, Sub_metering_1, type = "n",xlab= NA,ylab ="Energy sub metering"))
+with(DF, lines(datetime, Sub_metering_1, col = "black"))
+with(DF, lines(datetime, Sub_metering_2, col = "red"))
+with(DF, lines(datetime, Sub_metering_3, col = "blue"))
+legend("topright", lty = 1, col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# plot LR
+plot(DF$datetime,DF$Global_reactive_power,type = "l",ylab ="Global_reactive_power", xlab = "datetime")
+dev.off()
+
+
